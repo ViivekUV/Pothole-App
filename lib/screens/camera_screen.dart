eec0f6393a0c641b4c2d0bdad69e0177ed628c6c
Viivek Uppalapu <vivtimpot@gmail.com>
2020-07-23 18:40:48 +0530

    integrated ML model prediction of cat and dog

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tflite/tflite.dart';
 
 class CameraScreen extends StatefulWidget {
   static String id = 'camera_screen';
@@ -14,6 +15,11 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   /// Active image file
   File _imageFile;
+
+  List _outputs;
+  File _image;
+  bool _loading = false;
+
 
   final _auth = FirebaseAuth.instance;
   FirebaseUser loggedInUser;
@@ -43,6 +49,21 @@ class _CameraScreenState extends State<CameraScreen> {
     setState(() {
       _imageFile = selected;
     });
+    classifyImage(selected);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
   }
 
   /// Remove image
@@ -66,6 +87,13 @@ class _CameraScreenState extends State<CameraScreen> {
   void initState() {
     super.initState();
     getCurrentUser();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
   }
 
   @override
@@ -133,11 +161,30 @@ class _CameraScreenState extends State<CameraScreen> {
               child: Uploader(
                 file: _imageFile,
               ),
+            ),
+            Text(
+              "${_outputs[0]["label"]}",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                background: Paint()..color = Colors.white,
+              ),
             )
           ]
         ],
       ),
     );
+  }
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
   }
 }
 
@@ -202,7 +249,7 @@ class _UploaderState extends State<Uploader> {
                   Text(
                     '${(progressPercent * 100).toStringAsFixed(2)} % ',
                     style: TextStyle(fontSize: 50),
-                  ),
+                  ),      
                 ]);
           });
     } else {
@@ -228,21 +275,70 @@ class _UploaderState extends State<Uploader> {
 
 
 
-// chat screen
+
+
+
+
+
+
+
+
+
+
+
+
+//Original Code
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:image_cropper/image_cropper.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
 // class CameraScreen extends StatefulWidget {
-  
-//   @override
-//   _CameraScreenState createState() => _CameraScreenState();
+//   static String id = 'camera_screen';
+//   createState() => _CameraScreenState();
 // }
 
-// class _ChatScreenState extends State<ChatScreen> {
+// class _CameraScreenState extends State<CameraScreen> {
+//   /// Active image file
+//   File _imageFile;
+
 //   final _auth = FirebaseAuth.instance;
 //   FirebaseUser loggedInUser;
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     getCurrentUser();
+//   /// Cropper plugin
+//   Future<void> _cropImage() async {
+//     File cropped = await ImageCropper.cropImage(
+//         sourcePath: _imageFile.path,
+//         // ratioX: 1.0,
+//         // ratioY: 1.0,
+//         // maxWidth: 512,
+//         // // maxHeight: 512,
+//         // toolbarColor: Colors.purple,
+//         // toolbarWidgetColor: Colors.white,
+//         // toolbarTitle: 'Crop It'
+//     );
+
+//     setState(() {
+//       _imageFile = cropped ?? _imageFile;
+//     });
+//   }
+
+//   /// Select an image via gallery or camera
+//   Future<void> _pickImage(ImageSource source) async {
+//     File selected = await ImagePicker.pickImage(source: source);
+
+//     setState(() {
+//       _imageFile = selected;
+//     });
+//   }
+
+//   /// Remove image
+//   void _clear() {
+//     setState(() => _imageFile = null);
 //   }
 
 //   void getCurrentUser()async{
@@ -258,6 +354,12 @@ class _UploaderState extends State<Uploader> {
 //   }
 
 //   @override
+//   void initState() {
+//     super.initState();
+//     getCurrentUser();
+//   }
+
+//   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
@@ -270,42 +372,150 @@ class _UploaderState extends State<Uploader> {
 //                 Navigator.pop(context);
 //               }),
 //         ],
-//         title: Text('⚡️Chat'),
-//         backgroundColor: Colors.lightBlueAccent,
+//         title: Text('Capture Pothole'),
+//         backgroundColor: Colors.grey,
 //       ),
-//       body: SafeArea(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//       bottomNavigationBar: BottomAppBar(
+//         child: Row(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 //           children: <Widget>[
-//             Container(
-//               decoration: kMessageContainerDecoration,
-//               child: Row(
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: <Widget>[
-//                   Expanded(
-//                     child: TextField(
-//                       onChanged: (value) {
-//                         //Do something with the user input.
-//                       },
-//                       decoration: kMessageTextFieldDecoration,
-//                     ),
-//                   ),
-//                   FlatButton(
-//                     onPressed: () {
-//                       //Implement send functionality.
-//                     },
-//                     child: Text(
-//                       'Send',
-//                       style: kSendButtonTextStyle,
-//                     ),
-//                   ),
-//                 ],
+//             IconButton(
+//               icon: Icon(
+//                 Icons.photo_camera,
+//                 size: 30,
 //               ),
+//               onPressed: () => _pickImage(ImageSource.camera),
+//               color: Colors.blue,
+//             ),
+//             IconButton(
+//               icon: Icon(
+//                 Icons.photo_library,
+//                 size: 30,
+//               ),
+//               onPressed: () => _pickImage(ImageSource.gallery),
+//               color: Colors.pink,
 //             ),
 //           ],
 //         ),
 //       ),
+//       body: ListView(
+//         children: <Widget>[
+//           if (_imageFile != null) ...[
+//             Container(
+//                 padding: EdgeInsets.all(32), child: Image.file(_imageFile)),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: <Widget>[
+//                 FlatButton(
+//                   color: Colors.black,
+//                   child: Icon(Icons.crop),
+//                   onPressed: _cropImage,
+//                 ),
+//                 FlatButton(
+//                   color: Colors.black,
+//                   child: Icon(Icons.refresh),
+//                   onPressed: _clear,
+//                 ),
+//               ],
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.all(32),
+//               child: Uploader(
+//                 file: _imageFile,
+//               ),
+//             )
+//           ]
+//         ],
+//       ),
 //     );
 //   }
 // }
+
+// /// Widget used to handle the management of
+// class Uploader extends StatefulWidget {
+//   final File file;
+
+//   Uploader({Key key, this.file}) : super(key: key);
+
+//   createState() => _UploaderState();
+// }
+
+// class _UploaderState extends State<Uploader> {
+//   final FirebaseStorage _storage =
+//       FirebaseStorage(storageBucket: 'gs://pot-app-20aa2.appspot.com');
+
+//   StorageUploadTask _uploadTask;
+
+//   _startUpload() {
+//     String filePath = 'images/${DateTime.now()}.png';
+
+//     setState(() {
+//       _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (_uploadTask != null) {
+//       return StreamBuilder<StorageTaskEvent>(
+//           stream: _uploadTask.events,
+//           builder: (context, snapshot) {
+//             var event = snapshot?.data?.snapshot;
+
+//             double progressPercent = event != null
+//                 ? event.bytesTransferred / event.totalByteCount
+//                 : 0;
+
+//             return Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   if (_uploadTask.isComplete)
+//                     Text('🎉🎉🎉',
+//                         style: TextStyle(
+//                             color: Colors.greenAccent,
+//                             height: 2,
+//                             fontSize: 30)),
+//                   if (_uploadTask.isPaused)
+//                     FlatButton(
+//                       child: Icon(Icons.play_arrow, size: 50),
+//                       onPressed: _uploadTask.resume,
+//                     ),
+//                   if (_uploadTask.isInProgress)
+//                     FlatButton(
+//                       child: Icon(Icons.pause, size: 50),
+//                       onPressed: _uploadTask.pause,
+//                     ),
+
+                    
+//                   LinearProgressIndicator(value: progressPercent),
+//                   Text(
+//                     '${(progressPercent * 100).toStringAsFixed(2)} % ',
+//                     style: TextStyle(fontSize: 50),
+//                   ),
+//                 ]);
+//           });
+//     } else {
+//       return FlatButton.icon(
+//           color: Colors.blue,
+//           label: Text('Upload to Firebase'),
+//           icon: Icon(Icons.cloud_upload),
+//           onPressed: _startUpload);
+//     }
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
